e75eefd106f58a7974416d97e62b2d9dd6ab7ad4
Devender Reddy <41923640+Kdreddy2582@users.noreply.github.com>
2023-09-13 08:42:52 +0530

    main
    main

---
+++ main.js
@@ -0,0 +1,30 @@
+new Vue({
+  el: "#app",
+  data: {
+    message:
+      "Devender This is my V-Text Directive example Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur nobis, eius corporis adipisci nesciunt id labore. Nesciunt mollitia odio, nostrum fuga beatae vero excepturi, quaerat quisquam, consequatur iste ad aliquam.",
+  },
+});
+new Vue({
+  el: "#app2",
+  data:{
+    movieTicket: "The Akhanda"
+  }
+})
+
+new Vue({
+  el: "#myExpression",
+  data: {
+    isValid: false,
+  },
+});
+
+
+
+
+
+
+
+
+
+
